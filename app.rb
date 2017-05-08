e25339b485abdb0b13dbfa16a8fbd1d3a5ce969a
Ben Petrosky and Jesse Hoare Evans <ben+jesse@>
2017-05-08 11:07:34 -0700

    add active record, currently displays tasks, custom method done,notdone added to task.

--- app.rb
+++ app.rb
@@ -1,62 +1,33 @@
 require('sinatra')
 require('sinatra/reloader')
+require('sinatra/activerecord')
+also_reload('lib/**/*.rb')
 require('./lib/task')
 require('./lib/list')
-also_reload('lib/**/*.rb')
 require("pg")
-
-DB = PG.connect({:dbname => "to_do_test"})
 
 
 get("/") do
+  @tasks = Task.all()
   erb(:index)
 end
 
-get("/lists/new") do
-  erb(:list_form)
-end
-
-post("/lists_form") do
-  name = params.fetch("name")
-  list = List.new({:name => name, :id => nil})
-  list.save()
+post('/tasks') do
+  description = params.fetch('description')
+  task = Task.new({:description => description, :done => false})
+  task.save()
   erb(:success)
- end
-
- get('/lists') do
-  @lists = List.all()
-  erb(:lists)
 end
 
-get("/lists/:id") do
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list)
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch('id').to_i())
+  erb(:task_edit)
 end
 
-post("/tasks") do
+patch("/tasks/:id") do
   description = params.fetch("description")
-  list_id = params.fetch("list_id").to_i()
-  @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
-  erb(:success)
-end
-
-get('/lists/:id/edit') do
-  @list = List.find(params['id'].to_i())
-  erb(:list_edit)
-end
-
-patch('/lists/:id') do
-  name = params['name']
-  @list = List.find(params['id'].to_i())
-  @list.update({:name => name})
-  erb(:list)
-end
-
-delete("/lists/:id") do
-  @list = List.find(params.fetch("id").to_i())
-  @list.delete()
-  @lists = List.all()
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
   erb(:index)
 end
